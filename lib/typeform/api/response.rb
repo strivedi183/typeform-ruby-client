207c9b5079f062390b9c36f839cb6f1ebe1bdf31
CHOISEUNGYOUN <alchemist718@gmail.com>
2021-11-30 09:26:13 +0900

    fix page_size query params

--- lib/typeform/api/response.rb
+++ lib/typeform/api/response.rb
@@ -7,7 +7,7 @@ module Typeform # :nodoc:
       class << self
         # retrieve reponses by a form_id
         def retrieve_responses(form_id, page_size: 1000)
-          parse(JSON.parse(connection.get("forms/#{form_id}/responses?#{page_size}").body))
+          parse(JSON.parse(connection.get("forms/#{form_id}/responses?page_size=#{page_size}").body))
         end
       end
     end
