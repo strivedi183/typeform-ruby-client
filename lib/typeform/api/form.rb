22707d0813629abab348af1a2dd5623b0ed4c218
Jay9393 <tlswo1313@gmail.com>
2022-03-31 18:54:25 +0900

    feature: create-form 및 workspace 조회 기능 추가, faraday version down

--- lib/typeform/api/form.rb
+++ lib/typeform/api/form.rb
@@ -6,13 +6,18 @@ module Typeform # :nodoc:
     class Form < Client
       class << self
         # retrieve forms
-        def retrieve_forms(page_size: 200)
-          parse(JSON.parse(connection.get("forms?page_size=#{page_size}").body))
+        def retrieve_forms(workspace_id, page_size: 200, template: "Template")
+          parse(JSON.parse(connection.get("forms?page_size=#{page_size}&workspace_id=#{workspace_id}&search=#{template}").body))
         end
 
         # retrieve a form by a form_id
         def retrieve_form(form_id)
           parse(JSON.parse(connection.get("forms/#{form_id}").body))
+        end
+
+        def create_form(params)
+          res = connection.post("forms", params.to_json)
+          return JSON.parse(res.body.force_encoding("UTF-8"))["id"]
         end
       end
     end
--- test/typeform/client_test.rb
+++ test/typeform/client_test.rb
@@ -4,6 +4,6 @@ require "test_helper"
 
 class Typeform::ClientTest < Minitest::Test
   def test_that_it_has_a_version_number
-    assert_equal "0.1.1", ::Typeform::VERSION
+    assert_equal "0.1.2", ::Typeform::VERSION
   end
 end
